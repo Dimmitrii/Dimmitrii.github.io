98ebdb080b3b683e2304ed43fe4cf473a2bef664
Dima <dima.baturo1@gmail.com>
2020-05-28 22:09:59 +0300

    Add files via upload

---
+++ precache-manifest.cf166a9febd4b1859d236758230a7f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add2ef8ec302cb944cf3dd0fabcc0e31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f45e58d6860fcb3cc2f",
+    "url": "./static/css/main.39fc64e1.chunk.css"
+  },
+  {
+    "revision": "33c833482d877a5782d6",
+    "url": "./static/js/2.d44923eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d44923eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f45e58d6860fcb3cc2f",
+    "url": "./static/js/main.c2326758.chunk.js"
+  },
+  {
+    "revision": "cb716a7f8d2a5423406b",
+    "url": "./static/js/runtime-main.892f4492.js"
+  }
+]);
