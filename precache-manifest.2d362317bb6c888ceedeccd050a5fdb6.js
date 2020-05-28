53dda3504ad04149f095b244225c96355914cdc5
Dima <dima.baturo1@gmail.com>
2020-05-29 01:05:57 +0300

    Add files via upload

---
+++ precache-manifest.2d362317bb6c888ceedeccd050a5fdb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba2c232c20ed98a94b4f07e971f0ee6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ecfb7d0a9acd988bc81",
+    "url": "./static/css/main.39fc64e1.chunk.css"
+  },
+  {
+    "revision": "23593eb05f58e56c1921",
+    "url": "./static/js/2.7c667096.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.7c667096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ecfb7d0a9acd988bc81",
+    "url": "./static/js/main.42a8b7a8.chunk.js"
+  },
+  {
+    "revision": "cb716a7f8d2a5423406b",
+    "url": "./static/js/runtime-main.892f4492.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1c6577f9b2dc5ce4bf2f7184087b158f.js"
+  "./precache-manifest.2d362317bb6c888ceedeccd050a5fdb6.js"
 );
 
 self.addEventListener('message', (event) => {
